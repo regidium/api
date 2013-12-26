439462b0a8c8b5c02cb84f2de43b7b83578c8593
Alexey Volkov <alexey.wild88@gmail.com>
2013-12-27 01:12:23 +0200

    fix

--- app/cache/dev/annotations/be894bc4a007ec56035487335c049ba49af42731.cache.php
+++
@@ -1,1 +0,0 @@
-<?php return unserialize('a:1:{i:0;O:49:"Doctrine\\ODM\\MongoDB\\Mapping\\Annotations\\Document":7:{s:2:"db";N;s:10:"collection";s:5:"users";s:15:"repositoryClass";s:45:"Regidium\\UserBundle\\Repository\\UserRepository";s:7:"indexes";a:0:{}s:14:"requireIndexes";b:0;s:9:"slaveOkay";N;s:5:"value";N;}}');
