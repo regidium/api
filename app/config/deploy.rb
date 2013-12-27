25cacbf54eef021ad5ba8860044dd28e70568fd6
Alexey Volkov <alexey.wild88@gmail.com>
2013-12-27 11:29:10 +0200

    fix

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -16,31 +16,23 @@ default_run_options[:pty] = true
 
 set :keep_releases,         3
 set :ssh_options,           {:forward_agent => true, :port => 22}
-set :user,                  "root"
-set :use_sudo,              true
-set :use_composer,          true
-set :update_vendors,        true
-
-set :shared_files,          [
-                                "app/config/parameters.yml"
-                            ]
+set :user,                  "deployer"
+set :use_sudo,              false
+set :use_composer,          false
+set :update_vendors,        false
 
 set :shared_children,       [
                                 app_path + "/cache",
                                 app_path + "/logs",
+                                app_path + "/config",
                                 web_path + "/bundles",
-                                "vendor",
-                                "config"
+                                "vendor"
                             ]
 
-namespace :regidium do
-
-    task :permissions, :roles => :app do
-        run "sudo chown -R root:root #{release_path} #{release_path}/../../current"
-        run "sudo chmod -R 770 #{release_path} #{release_path}/../../current"
-        run "sudo chmod -R 777 #{release_path} #{release_path}/../../current/app/cache/"
-        run "sudo chmod -R 777 #{release_path} #{release_path}/../../current/app/cache/"
-        run "sudo service php5-fpm restart"
-    end
-
-end
+set :writable_dirs,         [
+                                app_path + "/cache",
+                                app_path + "/logs"
+                            ]
+set :webserver_user,        "www-data"
+set :permission_method,     :acl
+set :use_set_permissions,   false
